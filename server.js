e30aff11fc3400a98f5ce59e9bfe2156227caac5
Micola Vital <vitam1@bfh.ch>
2025-05-18 11:59:53 +0200

    docker deployment with GitHub actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Docker Deployment!');
+  res.send('Hello FS2025 DevOps Course! Docker Deployment with GitHub Actions!');
 });
 
 app.listen(PORT, HOST);
